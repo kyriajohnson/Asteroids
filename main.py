736bf44c08a486c350f5f08c096f1064b1a90151
kyriajohnson <xena.johnson@gmail.com>
2026-02-19 16:49:26 -0500

    Initial base files

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    while(True):
+        log_state()
+        #process the even queue
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        #refresh the screen
+        pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        
+
+
+if __name__ == "__main__":
+    main()

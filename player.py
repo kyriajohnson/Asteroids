acf65b86c0c6d8221f3d14a04d0c61206ebe50fc
kyriajohnson <xena.johnson@gmail.com>
2026-03-04 17:25:21 -0500

    Adding rate limiting to shots

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from circleshape import CircleShape
 from shot import Shot
 
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown_timer = 0
 
     # draw out player triangle
     def triangle(self):
@@ -25,6 +26,9 @@ class Player(CircleShape):
 
     # capture player input
     def update(self, dt):
+        # update shooting cooldown
+        self.cooldown_timer -= dt
+
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_a]:
@@ -51,8 +55,13 @@ class Player(CircleShape):
 
     # Hangle shooting
     def shoot(self):
+        if self.cooldown_timer > 0:
+            return
+        else:
+            self.cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
 
 
 

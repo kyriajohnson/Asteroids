106a1bdae742d4ac79e90ea065d147c8b5e9147e
kyriajohnson <xena.johnson@gmail.com>
2026-03-05 11:46:25 -0500

    Adding split mechanics for destroyed asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+from logger import log_event
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,26 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        # check if already smallest possible asteroid
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        # generate random angle for split off
+        angle = random.uniform(20, 50)
+        vec1 = self.velocity.rotate(angle)
+        vec2 = self.velocity.rotate(-angle)
+        
+        # create split asteroids
+        new_rad = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid1 = Asteroid(self.position.x, self.position.y, new_rad)
+        new_asteroid2 = Asteroid(self.position.x, self.position.y, new_rad)
+
+        # set new speeds
+        new_asteroid1.velocity = vec1 * 1.2
+        new_asteroid2.velocity = vec2 * 1.2
+
+        
